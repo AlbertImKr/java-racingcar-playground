e345060f43111d5cc5c6731f7a1b2b8394e9700e
Albert <guang7226961@naver.com>
2022-10-29 13:36:52 +0900

    feat(각 자동차에 이름을 부여할 수 있다.)

--- src/main/java/car_game/Car.java
+++ src/main/java/car_game/Car.java
@@ -1,9 +1,17 @@
 package car_game;
 
 public class Car {
-	public Car(String name) {
+
+	private String name;
+
+	public void setName(String name) {
 		if (name.length() > 5) {
 			throw new IllegalArgumentException("자동차 이름은 5자를 초과할 없다.");
 		}
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
 	}
 }
--- src/test/java/car_game/CarTest.java
+++ src/test/java/car_game/CarTest.java
@@ -2,13 +2,28 @@ package car_game;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CarTest {
 
+	private Car car;
+
+	@BeforeEach
+	void setCar(){
+		car = new Car();
+	}
+
 	@Test
 	void car_name_size() {
 		String name = "jackko";
-		assertThatThrownBy(() -> new Car(name)).hasMessage("자동차 이름은 5자를 초과할 없다.");
+		assertThatThrownBy(()->car.setName(name)).hasMessage("자동차 이름은 5자를 초과할 없다.");
+	}
+
+	@Test
+	void car_set_name(){
+		Car car = new Car();
+		car.setName("janko");
+		assertThat(car.getName()).isEqualTo("janko");
 	}
 }
